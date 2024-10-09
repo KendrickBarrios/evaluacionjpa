1850ad1fd2e219b3031247e557ce8fa3cb5c80b0
DmartzzC <dmartinezc@uamv.edu.ni>
2024-10-08 18:13:04 -0600

    Se agrega la clase Book

---
+++ src/main/Java/models/Book.java
@@ -0,0 +1,24 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@ToString
+public class Book {
+    @Id
+    private int Id;
+    private String Title;
+    private String Author;
+    private String PublishYear;
+    private boolean available;
+
+
+}
